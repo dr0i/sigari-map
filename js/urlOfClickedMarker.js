262069847a3bd9ae4b6c0018740ab2cafb5d4089
Pascal Christoph <christoph@hbz-nrw.de>
2020-03-11 12:36:17 +0100

    Switch to https also for the start screen

--- js/urlOfClickedMarker.js
+++ js/urlOfClickedMarker.js
@@ -1,10 +1,10 @@
 define([], function() {
   return function() {
     var markerUrl =
-      "http://gadel.org//images/stories/illus/2018-05-25/fundra-drache.jpg";
+      "https://gadel.org//images/stories/illus/2018-05-25/fundra-drache.jpg";
     var markerText = "Fundra";
     var markerLink =
-      "http://gadel.org/component/search/?searchword=fundra&ordering=newest&searchphrase=all&limit=0";
+      "https://gadel.org/component/search/?searchword=fundra&ordering=newest&searchphrase=all&limit=0";
     return {
       setMarkerUrl: function(url) {
         markerUrl = url;
